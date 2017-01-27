48cb0d9bb045f7b125310529cfac04ecd618e349
Barry Peterson <barry.peterson@rosetta.com>
2017-01-27 15:07:52 -0800

    ditch unneeded server

--- src/app.js
+++ src/app.js
@@ -7,21 +7,21 @@ import bodyParser from 'body-parser'
 // Import Routes
 import addUserRoutes from './routes/users'
 
-let app = new Express();
+let app = new Express()
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 // Add Routes
 app = addUserRoutes('/api/v1', app)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
 });
 
 // error handlers
@@ -32,23 +32,24 @@ if (app.get('env') === 'development') {
 **** Running in Development Mode ****
   `)
   app.use((err, req, res, next) => {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.json({
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.json({
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
-
-export default app
+const server = app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
